d27fbb15515b86bc1a388c82512193a8b4ebb1be
无名通讯科技 <34734036+203535211@users.noreply.github.com>
2026-02-12 11:22:34 +0800

    Add files via upload

--- constants/UpdateConfig.ts
+++ constants/UpdateConfig.ts
@@ -11,7 +11,7 @@ export const UPDATE_CONFIG = {
 
   // 获取平台特定的下载URL
   getDownloadUrl(version: string): string {
-    return `https://ghfast.top/https://github.com/203535211/YunDuoTV/releases/download/v${version}/orionTV.${version}.apk`;
+    return `https://ghfast.top/https://github.com/203535211/YunDuoTV/releases/download/v${version}/YunDuoTV.${version}.apk`;
   },
 
   // 是否显示更新日志
